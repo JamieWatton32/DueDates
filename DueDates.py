8f789b3af13c7e746f1a900d7cf67bd517fd952d
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-02-07 23:46:26 -0400

    Have tkinter buttons working. Need to better dynamically select whats due based on time

--- DueDates.py
+++ DueDates.py
@@ -1,13 +1,17 @@
 from datetime import datetime
 import csv
 import xlrd
+import tkinter as tk
+
+
 
 def CountDown(PythonTime):
    count = PythonTime - datetime.now()
    return count
 
-def Networking(CsvFile):
-   CsvReader = csv.DictReader(CsvFile)
+def Networking():
+   File = open("DueDates.csv","r")
+   CsvReader = csv.DictReader(File)
    for row in CsvReader:
       if row["NetTime"] == "":
          break 
@@ -16,32 +20,41 @@ def Networking(CsvFile):
       if PythonDate < datetime.now():
          pass
       else:
+          print(f'The next assignment for Networking is:\n')
           print(f'{row["Networking"]} is due at {PythonDate} or in {CountDown(PythonDate)}')
           break
         
 
    
-def WindowsAdmin(CsvFile):
-      CsvReader = csv.DictReader(CsvFile)
+def WindowsAdmin():
+      File = open("DueDates.csv","r")
+      CsvReader = csv.DictReader(File)
       for row in CsvReader:
          ExcelTime = float(row["OsysTime"])
          PythonDate = datetime(*xlrd.xldate_as_tuple(ExcelTime, 0))
          if PythonDate < datetime.now():
             pass
          else:
+            print(f'The next assignment for Windows Admin is:\n')
             print(f'{row["Networking"]} is due at {PythonDate} or in {CountDown(PythonDate)}')
+            
             break
 
 
 def main():
-   File = open("DueDates/DueDates.csv","r")
-   ClassName = input("Osys or Networking?").lower()
+
+   root = tk.Tk()
+   entry = tk.Entry(root)
+   entry.pack()
+
+   button1 = tk.Button(root, text="Osys", command=WindowsAdmin)
+   button2 = tk.Button(root, text="Networking", command=Networking)
+ 
+   button1.pack()
+   button2.pack()
+
+
+   root.mainloop()
    
-   if ClassName == "Networking":
-      Networking(File)
-   if ClassName == "Osys":
-      WindowsAdmin(File)
-
-
 if __name__ == "__main__":
    main()
